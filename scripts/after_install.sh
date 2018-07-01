1bfdc1b968e37ab46dfc4fca98d629389bcac1a8
Joe Kuczek <joe.kuczek@gmail.com>
2018-06-30 20:00:38 -0500

    update script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# Switch to the standard user
+su ec2-user
+
+# Install base Magento dependencies
 php /bin/composer.phar install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# Unmount the pub/media folder prior to removal
 if [ -d "/var/www/html/pub/media" ]; then
   umount /var/www/html/pub/media || true
 fi
 
+# Remove all web files
 rm -rf /var/www/html/* || true
 rm -rf /var/www/html/.* || true
