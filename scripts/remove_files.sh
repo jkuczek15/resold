e8dae7cd9a8b031435c84c2cc5570d644a950916
Joe Kuczek <joe.kuczek@gmail.com>
2018-06-30 19:40:54 -0500

    script update

--- scripts/remove_files.sh
+++ scripts/remove_files.sh
@@ -1,3 +1,8 @@
-umount /var/www/html/pub/media
-rm -rf /var/www/html/*
-rm -rf /var/www/html/.*
+#!/usr/bin/env bash
+
+if [ -d "/var/www/html/pub/media" ]; then
+  umount /var/www/html/pub/media || true
+fi
+
+rm -rf /var/www/html/* || true
+rm -rf /var/www/html/.* || true
