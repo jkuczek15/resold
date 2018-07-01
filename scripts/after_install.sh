9d37434b06663a11c546f3cf214acbd7bb2accee
Joe Kuczek <joe.kuczek@gmail.com>
2018-06-30 21:05:11 -0500

    here we go

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,3 +2,18 @@
 
 # Install base Magento dependencies
 php /bin/composer.phar install
+
+# Apply base magento configuration
+magento setup:config:set /
+--backend-frontname="stm"  /
+--session-save="files" /
+--db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" /
+--db-name="MagentoQuickstartDB" /
+--db-user="admin" /
+--db-password="Rootroot$"
+
+# Enable Magento modules
+magento module:enable --all
+
+# Compile Magento class files and inject dependencies
+magento setup:di:compile
