d1e9008dcfec39b7125e273f284c5326cba24c11
Joe Kuczek <joe.kuczek@gmail.com>
2018-06-30 21:20:34 -0500

    we are close

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,7 @@ if [ -d "/var/www/html/pub/media" ]; then
   umount /var/www/html/pub/media || true
 fi
 
-# Remove all web files
-rm -rf /var/www/html/* || true
-rm -rf /var/www/html/.* || true
+# Remove all relevant web files
+rm -rf /var/www/html/app || true
+rm -rf /var/www/html/pub || true
+rm -rf /var/www/html/var || true
