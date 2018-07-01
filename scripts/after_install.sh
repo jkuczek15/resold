1cdc3904cba4137b5b94513c104ff3b3bf105ccc
Joe Kuczek <joe.kuczek@gmail.com>
2018-06-30 23:37:26 -0500

    work please

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,7 +7,7 @@ php /bin/composer.phar install -d /var/www/html
 chmod -R 777 /var/www/html
 chmod +x /var/www/html/bin/magento
 
-# Apply Magento configuration for AWS cloud server
+# Install Magento and apply configuration for AWS cloud server
 /var/www/html/bin/magento setup:install \
 --backend-frontname="stm"  \
 --session-save="files" \
@@ -21,24 +21,9 @@ chmod +x /var/www/html/bin/magento
 --admin-user="joe" \
 --admin-password="Rootroot$" \
 --base-url="https://resold.us" \
+--use-secure="1" \
 --use-secure-admin="1" \
 --base-url-secure="1" \
 --language="en_US" \
 --currency="USD" \
 --timezone="America/Chicago"
-
-# Enable Magento modules
-/var/www/html/bin/magento module:enable --all
-
-# Upgrade Magento schema and modules
-/var/www/html/bin/magento setup:upgrade
-
-# Compile Magento class files and inject dependencies
-/var/www/html/bin/magento setup:di:compile
-
-# Grant read/write/execute permissions to extra folders
-chmod -R 777 /var/www/html/var/
-chmod -R 777 /var/www/html/app/etc/config.php /var/www/html/app/etc/env.php
-
-# Generate the Magento static content files
-/var/www/html/bin/magento setup:static-content:deploy
