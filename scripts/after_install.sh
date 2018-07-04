b93dc01f3edd8b7116a1406d1f9ed51d3f9df648
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-04 10:32:07 -0500

    deployment test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,41 +1,32 @@
 #!/usr/bin/env bash
 
 # Update Magento dependencies using composer
-php /bin/composer.phar update -d /var/www/html/setup
-
-# Install base Magento dependencies using composer
-php /bin/composer.phar install -d /var/www/html/setup
-
-# Update Magento dependencies using composer
 php /bin/composer.phar update -d /var/www/html
 
 # Install base Magento dependencies using composer
 php /bin/composer.phar install -d /var/www/html
 
-# Grant read/write/execute permissions to all web files
-chmod -R 777 /var/www/html
-
 # Install Magento and apply configuration for AWS cloud server
-/var/www/html/bin/magento setup:install \
---backend-frontname="stm"  \
---session-save="files" \
---db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" \
---db-name="MagentoQuickstartDB" \
---db-user="admin" \
---db-password="Rootroot$" \
---admin-firstname="Joe" \
---admin-lastname="Kuczek" \
---admin-email="joe.kuczek@gmail.com" \
---admin-user="joe" \
---admin-password="Bigjoe3092$" \
---base-url="https://resold.us" \
---base-url-secure="https://resold.us" \
---use-secure="1" \
---use-secure-admin="1" \
---use-rewrites="1" \
---language="en_US" \
---currency="USD" \
---timezone="America/Chicago"
+# /var/www/html/bin/magento setup:install \
+# --backend-frontname="stm"  \
+# --session-save="files" \
+# --db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" \
+# --db-name="MagentoQuickstartDB" \
+# --db-user="admin" \
+# --db-password="Rootroot$" \
+# --admin-firstname="Joe" \
+# --admin-lastname="Kuczek" \
+# --admin-email="joe.kuczek@gmail.com" \
+# --admin-user="joe" \
+# --admin-password="Bigjoe3092$" \
+# --base-url="https://resold.us" \
+# --base-url-secure="https://resold.us" \
+# --use-secure="1" \
+# --use-secure-admin="1" \
+# --use-rewrites="1" \
+# --language="en_US" \
+# --currency="USD" \
+# --timezone="America/Chicago"
 
 # Grant read/write/execute permissions to all web files
 chmod -R 777 /var/www/html
@@ -43,23 +34,14 @@ chmod -R 777 /var/www/html
 # Enable all modules
 /var/www/html/bin/magento module:enable --all --clear-static-content
 
-# Grant read/write/execute permissions to all web files
-chmod -R 777 /var/www/html
-
 # Upgrade Magento module schema
 /var/www/html/bin/magento setup:upgrade || true
-
-# Grant read/write/execute permissions to all web files
-chmod -R 777 /var/www/html
 
 # Compile Magento class files and inject dependencies
 /var/www/html/bin/magento setup:di:compile || true
 
 # Deploy Magento static content
 /var/www/html/bin/magento setup:static-content:deploy || true
-
-# Grant read/write/execute permissions to all web files
-chmod -R 777 /var/www/html
 
 # Overwrite default vendor files
 rsync -a /var/www/html/vendor_override/ /var/www/html/vendor/
