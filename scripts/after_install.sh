d19eab8497ccad0e42e78951dad1ceef409d3aa3
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-04 12:05:14 -0500

    fucking work

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,17 +3,14 @@
 # Grant read/write/execute permissions to all web files
 chmod -R 777 /var/www/html
 
-# Update Magento dependencies using composer
-composer update -d /var/www/html
-
-# Install Magento dependencies using composer
-composer install -d /var/www/html
+# Update and Install Magento dependencies using composer
+php /bin/composer.phar update -d /var/www/html
 
 # Make the Magento cli tool executable
 chmod +x /var/www/html/bin/magento
 
 # Install Magento and apply configuration for AWS cloud server
-magento setup:install \
+/var/www/html/bin/magento setup:install \
 --backend-frontname="stm"  \
 --session-save="files" \
 --db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" \
@@ -35,13 +32,13 @@ magento setup:install \
 --timezone="America/Chicago"
 
 # Upgrade Magento module schema
-magento setup:upgrade
+/var/www/html/bin/magento setup:upgrade
 
 # Compile Magento class files and inject dependencies
-magento setup:di:compile
+/var/www/html/bin/magento setup:di:compile
 
 # Deploy Magento static content
-magento setup:static-content:deploy
+/var/www/html/bin/magento setup:static-content:deploy
 
 # Overwrite default vendor files
 rsync -a /var/www/html/vendor_override/ /var/www/html/vendor/
