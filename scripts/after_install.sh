b77e8b4c0d674b2056cf4369d3ffa962ce46ee98
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-04 11:23:55 -0500

    site should work

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,38 +1,35 @@
 #!/usr/bin/env bash
 
-# Update Magento dependencies using composer
-php /bin/composer.phar update -d /var/www/html
-
-# Install base Magento dependencies using composer
-php /bin/composer.phar install -d /var/www/html
-
-# Install Magento and apply configuration for AWS cloud server
-# /var/www/html/bin/magento setup:install \
-# --backend-frontname="stm"  \
-# --session-save="files" \
-# --db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" \
-# --db-name="MagentoQuickstartDB" \
-# --db-user="admin" \
-# --db-password="Rootroot$" \
-# --admin-firstname="Joe" \
-# --admin-lastname="Kuczek" \
-# --admin-email="joe.kuczek@gmail.com" \
-# --admin-user="joe" \
-# --admin-password="Bigjoe3092$" \
-# --base-url="https://resold.us" \
-# --base-url-secure="https://resold.us" \
-# --use-secure="1" \
-# --use-secure-admin="1" \
-# --use-rewrites="1" \
-# --language="en_US" \
-# --currency="USD" \
-# --timezone="America/Chicago"
-
 # Grant read/write/execute permissions to all web files
 chmod -R 777 /var/www/html
 
-# Enable all modules
-/var/www/html/bin/magento module:enable --all --clear-static-content
+# Update Magento dependencies using composer
+php /bin/composer.phar update -d /var/www/html
+
+# Make the Magento cli tool executable
+chmod +x /var/www/html/bin/magento
+
+# Install Magento and apply configuration for AWS cloud server
+/var/www/html/bin/magento setup:install \
+--backend-frontname="stm"  \
+--session-save="files" \
+--db-host="mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com" \
+--db-name="MagentoQuickstartDB" \
+--db-user="admin" \
+--db-password="Rootroot$" \
+--admin-firstname="Joe" \
+--admin-lastname="Kuczek" \
+--admin-email="joe.kuczek@gmail.com" \
+--admin-user="joe" \
+--admin-password="Bigjoe3092$" \
+--base-url="https://resold.us" \
+--base-url-secure="https://resold.us" \
+--use-secure="1" \
+--use-secure-admin="1" \
+--use-rewrites="1" \
+--language="en_US" \
+--currency="USD" \
+--timezone="America/Chicago"
 
 # Upgrade Magento module schema
 /var/www/html/bin/magento setup:upgrade || true
@@ -44,7 +41,4 @@ chmod -R 777 /var/www/html
 /var/www/html/bin/magento setup:static-content:deploy || true
 
 # Overwrite default vendor files
-rsync -a /var/www/html/vendor_override/ /var/www/html/vendor/
-
-# Enable developer mode
-/var/www/html/bin/magento deploy:mode:set developer
+rsync -a /var/www/html/vendor_override/ /var/www/html/vendor/ || true
