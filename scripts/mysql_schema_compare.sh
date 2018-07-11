7ba7830c078c32c66e30822bed3dd0b98758aa0b
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-10 19:27:30 -0500

    Add scripts to compare two MySQL databases

---
+++ scripts/mysql_schema_compare.sh
@@ -0,0 +1,6 @@
+##########################################################################
+##########################################################################
+# Runs a schema database comparison between localhost and AWS servers
+##########################################################################
+##########################################################################
+mysqldbcompare --server1=root:"Rootroot$"@localhost:3306:/var/run/mysqld/mysqld.sock --server2=admin:"Rootroot$"@"mm6imdf4u5ak4w.czqsdryzxcba.us-west-2.rds.amazonaws.com":3306:/var/run/mysqld/mysqld.sock MagentoQuickstartDB:MagentoQuickstartDB
