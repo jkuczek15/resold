19da674c7f44e23b13485f3b6f93dffe7e8030fb
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-28 14:00:21 -0500

    Add vendor override script

---
+++ scripts/override.sh
@@ -0,0 +1,11 @@
+file=$1
+
+if [[ -n "$file" ]]; then
+    # Change to the vendor directory
+    cd /var/www/html/vendor/
+
+    # Copy the file along with folder structure
+    cp --parents $1 /var/www/html/vendor/override
+else
+    echo "Please provide a vendor module path as an argument. Ex. magento/module-paypal/Controller/Payflow.php"
+fi
