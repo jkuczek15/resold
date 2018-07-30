a46e4808a015f0effac2ada376b190fbb8819e37
Joe Kuczek <joe.kuczek@gmail.com>
2018-07-29 20:11:55 -0500

    Image upload working

--- app/code/Custom/Api/Controller/Product/Index.php
+++ app/code/Custom/Api/Controller/Product/Index.php
@@ -52,11 +52,10 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-      // FIXME: Before launch lockdown API using the code below
-      // if (!$this->session->isLoggedIn()) {
-      //   $resultRedirect = $this->resultRedirectFactory->create();
-      //   return $resultRedirect->setPath('market/account/login');
-      // }
+      $resultRedirect = $this->resultRedirectFactory->create();
+      if (!$this->session->isLoggedIn()) {
+        return $resultRedirect->setPath('customer/account/login');
+      }
 
       $post = $this->getRequest()->getPostValue();
 
@@ -68,9 +67,9 @@ class Index extends \Magento\Framework\App\Action\Action
       $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
       $_product = $objectManager->create('\Magento\Catalog\Model\Product');
 
-      // TODO: Add server side validation
+      // TODO: Add server side validation for raw data
       // Create a unique product ID and save product to database
-      $sku = uniqid ("product-", true);
+      $sku = uniqid("product-", true);
       $_product = $objectManager->create('Magento\Catalog\Model\Product');
       $_product->setName($post['name']);
       $_product->setSku($sku);
@@ -84,10 +83,39 @@ class Index extends \Magento\Framework\App\Action\Action
       $_product->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
       $_product->setWebsiteIds(array(1));
       $_product->setCustomAttribute('condition', $post['condition']);
+      $_product->setStockData(['qty' => 1, 'is_in_stock' => true]);
+
+      // TODO: Add service side validation for images
+      // tempory location for product images
+      $mediaDir = '/var/www/html/pub/media';
+      $images = $_FILES['images']['name'];
+
+      foreach($images as $key => $image)
+      {
+          // get temporary location of image and image extension
+          $tmpPath = $_FILES['images']['tmp_name'][$key];
+
+          if($tmpPath != '')
+          {
+            $extension = pathinfo($image, PATHINFO_EXTENSION);
+
+            // new path for the image stored in the media directory
+            $newPath = $mediaDir.$tmpPath.'.'.$extension;
+
+            // move the uploaded image to the media directory
+            rename($tmpPath, $newPath);
+
+            // link the image to the product and upload it to the S3 bucket
+            $_product->addImageToMediaGallery($newPath, array('image', 'small_image', 'thumbnail'), false, false);
+
+            // remove the image from our file Filesystem
+            unlink($newPath);
+          }// end if we have a temp file path
+      }// end foreach loop over images
 
       // save the product to the database
       $_product->save();
 
-      return $this->resultJsonFactory->create()->setData(['id' => $sku]);
+      return $resultRedirect->setPath('customer/account');
     }// end function execute
 }
