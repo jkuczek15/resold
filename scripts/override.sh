f1fe1e4ddc7d3a4094a7548d2b7fc8f3cb2f05b3
Joe Kuczek <joe.kuczek@gmail.com>
2018-08-04 17:44:19 -0500

    fix the override script

--- scripts/override.sh
+++ scripts/override.sh
@@ -5,7 +5,7 @@ if [[ -n "$file" ]]; then
     cd /var/www/html/vendor/
 
     # Copy the file along with folder structure
-    cp --parents $1 /var/www/html/vendor/override
+    cp --parents $1 /var/www/html/vendor/resold
 else
     echo "Please provide a vendor module path as an argument. Ex. magento/module-paypal/Controller/Payflow.php"
 fi
