65f6004c9853d51e5e57dad72d2001dd1e437775
Joe Kuczek <joe.kuczek@gmail.com>
2018-08-04 16:53:50 -0500

    Remove mageplaza icons

--- scripts/local_after_install.sh
+++ scripts/local_after_install.sh
@@ -37,11 +37,11 @@ sudo chmod +x /var/www/html/bin/magento
 # Compile Magento class files and inject dependencies
 /var/www/html/bin/magento setup:di:compile
 
+# Copy merge vendor override Filesystem
+rsync -a /var/www/html/vendor/override/* /var/www/html/vendor/
+
 # Deploy Magento static content
 /var/www/html/bin/magento setup:static-content:deploy
 
 # Grant permissions to all web files
 sudo chmod -R 777 /var/www/html
-
-# Copy merge vendor override Filesystem
-rsync -a /var/www/html/vendor/override/* /var/www/html/vendor/
