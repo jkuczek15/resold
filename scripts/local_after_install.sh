ec8c9d01782fe75872067aa162a711e8b795a64e
Joe Kuczek <joe.kuczek@gmail.com>
2018-08-11 12:46:06 -0500

    Fix cart issue

--- scripts/local_after_install.sh
+++ scripts/local_after_install.sh
@@ -9,6 +9,9 @@ composer update -d /var/www/html
 # Make the Magento command line tool executable
 sudo chmod +x /var/www/html/bin/magento
 
+# Copy merge vendor override Filesystem
+rsync -a /var/www/html/vendor/resold/* /var/www/html/vendor/
+
 # Install Magento and apply configuration for AWS cloud server
 /var/www/html/bin/magento setup:install \
 --backend-frontname="stm"  \
@@ -39,9 +42,6 @@ sudo chmod +x /var/www/html/bin/magento
 
 # Deploy Magento static content
 /var/www/html/bin/magento setup:static-content:deploy
-
-# Copy merge vendor override Filesystem
-rsync -a /var/www/html/vendor/resold/* /var/www/html/vendor/
 
 # Grant permissions to all web files
 sudo chmod -R 777 /var/www/html
