ec7ca45f076292bd07fefc088bd4676e5a913228
Joe Kuczek <joe.kuczek@gmail.com>
2018-08-19 12:32:27 -0500

    Cleanup account listing page

--- app/code/Ced/CsMarketplace/Controller/Vshops/View.php
+++ app/code/Ced/CsMarketplace/Controller/Vshops/View.php
@@ -24,13 +24,13 @@ use Magento\Framework\View\Result\PageFactory;
 
 class View extends \Magento\Framework\App\Action\Action
 {
-    
+
     protected $_coreRegistry;
-    
+
     protected $resultForwardFactory;
-    
-    protected $resultPageFactory;    
-    
+
+    protected $resultPageFactory;
+
     public function __construct(
         Context $context,
         PageFactory $resultPageFactory,
@@ -42,31 +42,31 @@ class View extends \Magento\Framework\App\Action\Action
         $this->resultForwardFactory = $resultForwardFactory;
         parent::__construct($context);
     }
-    
+
     protected function _initVendor()
-    {    
+    {
         $this->_eventManager->dispatch('csmarketplace_controller_vshops_init_before', array('controller_action' => $this));
-        
+
         if(!$this->_objectManager->get('Ced\CsMarketplace\Helper\Acl')->isEnabled()) {
-            return false; 
+            return false;
         }
-        
+
         $shopUrl = $this->getRequest()->getParam('shop_url');
         if (!strlen($shopUrl)) {
             return false;
         }
         $storeId = $this->_objectManager->get('Ced\CsMarketplace\Helper\Data')->getStore()->getId();
-        
+
         $vendor = $this->_objectManager->get('Ced\CsMarketplace\Model\Vendor')
                     ->setStoreId($storeId)->loadByAttribute('shop_url', $shopUrl);
-        
+
         if (!$this->_objectManager->get('Ced\CsMarketplace\Helper\Data')->canShow($vendor)) {
             return false;
         } else if(!$this->_objectManager->get('Ced\CsMarketplace\Helper\Data')->isShopEnabled($vendor)) {
             return false;
         }
         $this->_coreRegistry->register('current_vendor', $vendor);
-     
+
         try {
             $this->_eventManager->dispatch(
                 'csmarketplace_controller_vshops_init_after',
@@ -77,7 +77,7 @@ class View extends \Magento\Framework\App\Action\Action
             );
         } catch (\Exception $e) {
             $this->messageManager->addErrorMessage(__('Invalid login or password.'));
-        } 
+        }
         return $vendor;
     }
 
@@ -88,7 +88,7 @@ class View extends \Magento\Framework\App\Action\Action
     {
         // product filter start
         $data = $this->getRequest()->getParams();
-        if (isset($data['product_list_dir'])) { 
+        if (isset($data['product_list_dir'])) {
             $this->_coreRegistry->register('name_filter', $data['product_list_dir']);
         }
         // product filter end
@@ -100,7 +100,7 @@ class View extends \Magento\Framework\App\Action\Action
                             ->load($helper->getRootId());
             }
             $resultPage = $this->resultPageFactory->create();
-            $resultPage->getConfig()->getTitle()->set(__($vendor->getPublicName()." ".('Shop')));
+            $resultPage->getConfig()->getTitle()->set(__($vendor->getPublicName()));
             return $resultPage;
         }
         $this->messageManager->addErrorMessage(__('The Vendor\'s Shop you are trying to access is not available at this moment.'));
