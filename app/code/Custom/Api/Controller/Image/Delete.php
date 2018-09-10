46b76075d8b82144ac88b0f0abb7d75cccb8a479
Joe Kuczek <joe.kuczek@gmail.com>
2018-09-09 23:44:42 -0500

    Delete images on edit product

--- app/code/Custom/Api/Controller/Image/Delete.php
+++ app/code/Custom/Api/Controller/Image/Delete.php
@@ -52,15 +52,28 @@ class Delete extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
+      $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
       // tempory location for product images
       $mediaDir = '/var/www/html/pub/media';
 
       if(isset($_POST['qquuid']) && $_POST['qquuid'] != null)
       {
           $tmpPathExt = $_POST['qquuid'];
-          unlink($mediaDir.$tmpPathExt);
+
+          if(strpos($tmpPathExt, "/tmp") !== FALSE){
+            // deleting a temporary file on the server
+            unlink($mediaDir.$tmpPathExt);
+          }else if(isset($_POST['product_id']) && $_POST['product_id'] != null){
+            // deleting and unlinking permanent product gallery images
+            $product_id = $_POST['product_id'];
+            $product = $objectManager->create('Magento\Catalog\Model\Product')->load($product_id);
+            $imageProcessor = $objectManager->create('\Magento\Catalog\Model\Product\Gallery\Processor');
+            $imageProcessor->removeImage($product, $tmpPathExt);
+          }// end if temporary image directory
+
       }// end if temp file path is set
 
+      $product->save();
       return $this->resultJsonFactory->create()->setData(['success' => 'Y', 'path' => $tmpPathExt]);
     }// end function execute
 }
