86da8b3941245a23adde8a927cc0ae31c61a6417
Joe Kuczek <joe.kuczek@gmail.com>
2018-10-07 16:19:58 -0500

    Fixing up checkout flow

---
+++ vendor/resold/magento/module-checkout/view/frontend/web/js/view/progress-bar.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*jshint browser:true jquery:true*/
+/*global alert*/
+define(
+    [
+        'jquery',
+        "underscore",
+        'ko',
+        'uiComponent',
+        'Magento_Checkout/js/model/step-navigator',
+        'jquery/jquery.hashchange'
+    ],
+    function ($, _, ko, Component, stepNavigator) {
+        var steps = stepNavigator.steps;
+
+        return Component.extend({
+            defaults: {
+                template: 'Magento_Checkout/progress-bar',
+                visible: true
+            },
+            steps: steps,
+
+            initialize: function() {
+                this._super();
+                $(window).hashchange(_.bind(stepNavigator.handleHash, stepNavigator));
+                stepNavigator.handleHash();
+            },
+
+            sortItems: function(itemOne, itemTwo) {
+                return stepNavigator.sortItems(itemOne, itemTwo);
+            },
+
+            navigateTo: function(step) {
+                if(step.code == 'shipping'){
+                  if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                    $('.modal-custom').css('cssText', 'margin-top: 234px;');
+                  }
+                }
+                stepNavigator.navigateTo(step.code);
+            },
+
+            isProcessed: function(item) {
+                return stepNavigator.isProcessed(item.code);
+            }
+        });
+    }
+);
