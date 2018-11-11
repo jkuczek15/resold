c602d30100991e8005bf44c0152f6bfdce7fcfa3
Joe Kuczek <joe.kuczek@gmail.com>
2018-11-11 15:07:35 -0600

    Add refund collected fee webhook

--- app/code/Custom/Api/Controller/Refund/Index.php
+++ app/code/Custom/Api/Controller/Refund/Index.php
@@ -74,9 +74,52 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-      // Ensure POST request
-      $post = $this->getRequest()->getPostValue();
+      if($_SERVER['HTTP_USER_AGENT'] !== 'Stripe/1.0 (+https://stripe.com/docs/webhooks)'){
+        return $this->resultJsonFactory->create()->setData(['error' => 'Invalid Request']);
+      }
+
+      // Retrieve the request's body and parse it as JSON:
+      $input = @file_get_contents('php://input');
+      $event_json = json_decode($input, true);
+
+      $ob = $this->_objectManager;
+      $store = $ob->get('Magento\Framework\App\Config\ScopeConfigInterface');
+      $clientIdMode = $store->getValue('payment/ced_csstripe_method_one/client_id_mode');
+
+      if($clientIdMode=='Development'){
+        $clientId = $store->getValue('payment/ced_csstripe_method_one/client_did');
+      }else{
+        $clientId = $store->getValue('payment/ced_csstripe_method_one/client_pid');
+      }
+
+      /** get API mode test/live */
+      $mode = $store->getValue('payment/ced_csstripe_method_one/gateway_mode');
+
+      $skey = "api_{$mode}_secret_key";
+      $key = $store->getValue('payment/ced_csstripe_method_one/'.$skey);
+
+      // get the charge id from refund request
+      $charge_id = $event_json['data']['object']['id'];
+
+      $i = 0;
+      do {
+        $success = false;
+        try {
+            \Stripe\Stripe::setApiKey($key);
+            $fee_id = \Stripe\ApplicationFee::all(["charge" => $charge_id])->data[0]['id'];
+            $fee = \Stripe\ApplicationFee::retrieve($fee_id);
+            $fee->refunds->create();
+            $success = true;
+        } catch (\Stripe\Error\Base $e) {
+            // Catch Stripe exceptions
+        } catch (Exception $e) {
+          // Catch any other non-Stripe exceptions
+        }
+
+        if($success){ break; }
+        $i++;
+      } while($i < 10);
 
       return $this->resultJsonFactory->create()->setData(['success' => 'Y']);
     }// end function execute
-}
+  }
