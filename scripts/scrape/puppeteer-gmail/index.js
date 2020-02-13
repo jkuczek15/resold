aecb575bdade825bb030aa0fac6ec10094d3b3d3
Joe Kuczek <joe@resold.us>
2020-02-12 20:22:18 -0600

    Scraping updates

--- scripts/scrape/puppeteer-gmail/index.js
+++ scripts/scrape/puppeteer-gmail/index.js
@@ -4,17 +4,40 @@ const fs = require("fs");
 const path = require("path");
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 
-const posts = fs
+let getRandom = (arr) => {
+  return arr[Math.floor(Math.random()*arr.length)];
+};
+
+let filter = (arr) => {
+  return arr.filter(function (el) {
+    return el != null;
+  });
+};
+
+const posts = filter(fs
   .readFileSync(path.join(__dirname, "./", `resources/posts-list.txt`), "utf8")
-  .split("\n");
+  .split("\n"));
 
 /* Customizable variables */
-const subject = "Your listing on Craigslist";
-const message =
-`Hello,
-I hope you're having a great day.
-Kind regards,
-Joe.`;
+const emailStarters = filter(fs
+  .readFileSync(path.join(__dirname, "./", `resources/email-starters.txt`), "utf8")
+  .split("\n"));
+
+const emailBodys = filter(fs
+  .readFileSync(path.join(__dirname, "./", `resources/email-body.txt`), "utf8")
+  .split("\n"));
+
+const emailClosers = filter(fs
+  .readFileSync(path.join(__dirname, "./", `resources/email-closers.txt`), "utf8")
+  .split("\n"));
+
+const emailNames = filter(fs
+  .readFileSync(path.join(__dirname, "./", `resources/email-names.txt`), "utf8")
+  .split("\n"));
+
+const emailSubjects = filter(fs
+  .readFileSync(path.join(__dirname, "./", `resources/email-subjects.txt`), "utf8")
+  .split("\n"));
 
 (async () => {
   puppeteer.use(pluginStealth());
@@ -29,16 +52,22 @@ Joe.`;
   await emailSender.login(page);
 
   for (let i = lastEmailIndex; i < posts.length; i++) {
-
     let post = posts[i].split(",");
 
-    // todo: dynamically build email message based on this stuff + random emails
+    // todo: dynamically build email message based on this stuff
     let email = post[0];
-    let title = post[1];
-    let price = post[2];
-    let location = post[3];
-    let timeago = post[4];
-    let url = post[5];
+    // let title = post[1];
+    // let price = post[2];
+    // let location = post[3];
+    // let timeago = post[4];
+    // let url = post[5];
+
+    let subject = getRandom(emailSubjects);
+    let greeting = getRandom(emailStarters);
+    let body = getRandom(emailBodys);
+    let closing = getRandom(emailClosers);
+    let name = getRandom(emailNames);
+    let message = `${greeting}\r\n${body}\r\n${closing}\r\n${name}`;
 
     await emailSender.writeNewEmail(page, {
       index: i,
