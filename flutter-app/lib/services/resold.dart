34de385cfdfb775800244b28aa4f5eb2e2ea9df4
Joe Kuczek <joe@resold.us>
2020-07-21 21:21:21 -0500

    local fetch

--- flutter-app/lib/services/resold.dart
+++ flutter-app/lib/services/resold.dart
@@ -6,7 +6,7 @@ class Api {
 
   static final searchUrl = 'https://search-resold-es-iy75wommvnfqf5hf7p6w7ej2sq.us-west-2.es.amazonaws.com';
   static final searchIndex = 'magento*';
-  static final searchType = 'doc';
+  static final searchType = 'geo_point';
 
   static final transport = ConsoleHttpTransport(Uri.parse(searchUrl));
   static final client = elastic.Client(transport);
@@ -15,33 +15,30 @@ class Api {
   static Future<List<Product>> fetchLocalProducts(double latitude, double longitude, {int offset = 0}) async {
 
     var query = elastic.Query.bool(
-        must: elastic.Query.matchAll() ,
-        filter: {
-          "geo_bounding_box": {
-            "location": {
-              "top_left": {
-                "lat": 42,
-                "lon": -72
-              },
-              "bottom_right": {
-                "lat": 40,
-                "lon": -74
-              }
-            }
-          }
-
-        'ignore_mapped': true,
+      must: elastic.Query.matchAll(),
+      filter: {
         'geo_distance': {
-          'distance': '200km',
-          'pin.location': {
-            'lat': '${latitude}',
-            'lon' : '${longitude}'
+          'distance': '50mi',
+          'location_raw': {
+            'lat': latitude,
+            'lon' : longitude
           }
         }
       }
     );
-    
-    final searchResults = await client.search(searchIndex, searchType, query, source: true, offset: offset, limit: itemsPerPage);
+
+    var sort = [{
+      '_geo_distance' : {
+        'location_raw': {
+          'lat': latitude,
+          'lon' : longitude
+        },
+        'order' : 'asc',
+        'unit' : 'mi'
+      }
+    }];
+
+    final searchResults = await client.search(searchIndex, searchType, query, source: true, offset: offset, limit: itemsPerPage, sort: sort);
 
     List<Product> products = new List<Product>();
     searchResults.hits.forEach((doc) => products.add(Product.fromDoc(doc.doc)));
