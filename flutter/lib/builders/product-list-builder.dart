49b5eec85169982344e52c24d53e414c4722569d
Joe Kuczek <joe.kuczek@gmail.com>
2020-07-30 21:28:13 -0500

    product detail view

--- flutter/lib/builders/product-list-builder.dart
+++ flutter/lib/builders/product-list-builder.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:resold/constants/ui-constants.dart';
+import 'package:resold/constants/url-config.dart';
 import 'package:resold/view-models/product-view-model.dart';
 import 'package:resold/models/product.dart';
 import 'package:resold/widgets/creation-aware-list-item.dart';
+import 'package:resold/screens/product/view.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
@@ -11,11 +13,9 @@ import 'package:geolocator/geolocator.dart';
 
 class ProductListBuilder {
 
-  static String baseImagePath = 'https://s3-us-west-2.amazonaws.com/resold-photos/catalog/product';
-
-  static ChangeNotifierProvider<ProductViewModel> buildProductList(List<Object> data, Position currentLocation) {
+  static ChangeNotifierProvider<ProductViewModel> buildProductList(BuildContext context, List<Product> products, Position currentLocation) {
     return ChangeNotifierProvider<ProductViewModel> (
-      create: (_) => new ProductViewModel(currentLocation, data),
+      create: (_) => new ProductViewModel(currentLocation, products),
       child: Consumer<ProductViewModel> (
         builder: (context, model, child) => ListView.builder(
           itemCount: model.items.length,
@@ -25,27 +25,27 @@ class ProductListBuilder {
                 children: [
                   SizedBox(height: 10),
                   SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                          children: <Widget>[
-                            Padding(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
+                          ),
+                          Padding(
                               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                               child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
-                            ),
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
-                            ),
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
-                            ),
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
-                            ),
-                          ]
-                      )
+                          ),
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
+                          ),
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              child: Image.asset('assets/images/categories/electronics.jpg', height: 175)
+                          ),
+                        ]
+                    )
                   )
                 ]
               );
@@ -56,7 +56,7 @@ class ProductListBuilder {
                 SchedulerBinding.instance.addPostFrameCallback((duration) => model.handleItemCreated(index));
               },
               child: model.items[index+1].name == LoadingIndicatorTitle ?
-              Center(child: CircularProgressIndicator(backgroundColor: const Color(0xff41b8ea))) : buildProductTile(currentLocation, model.items[index], index)
+              Center(child: CircularProgressIndicator(backgroundColor: const Color(0xff41b8ea))) : buildProductTile(context, currentLocation, model.items[index], index)
             );
           }
         ),
@@ -64,7 +64,7 @@ class ProductListBuilder {
     );
   }
 
-  static ListTile buildProductTile(Position currentLocation, Product data, int index) {
+  static ListTile buildProductTile(BuildContext context, Position currentLocation, Product product, int index) {
     var formatter = new NumberFormat("\$###,###", "en_US");
     return ListTile(
         title: Card(
@@ -76,72 +76,75 @@ class ProductListBuilder {
                 child: Container (
                   padding: EdgeInsets.fromLTRB(25, 25, 25, 25),
                   child: Column (
-                      children: [
-                        Row (
-                            children: [
-                              Column(
-                                  children: [
-                                    Align(
-                                        alignment: Alignment.center,
-                                        child: SizedBox (
-                                            height: 270,
-                                            width: 270,
-                                            child: FadeInImage(image: NetworkImage(baseImagePath + data.thumbnail), placeholder: AssetImage('assets/images/placeholder-image.png'), fit: BoxFit.cover)
-                                        )
-                                    ),
-                                    SizedBox(height: 5),
-                                  ]
-                              )
-                            ]
-                        ),
-                        Row (
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      padding: new EdgeInsets.only(right: 13.0),
-                                      width: 200,
-                                      child: new Text(
-                                        data.name,
-                                        overflow: TextOverflow.fade,
-                                        style: new TextStyle(
-                                          fontSize: 14.0,
-                                          fontFamily: 'Roboto',
-                                          fontWeight: FontWeight.normal,
-                                        ),
+                    children: [
+                      Row (
+                          children: [
+                            Column(
+                                children: [
+                                  Align(
+                                      alignment: Alignment.center,
+                                      child: SizedBox (
+                                        height: 270,
+                                        width: 270,
+                                        child: FadeInImage(image: NetworkImage(baseImagePath + product.thumbnail), placeholder: AssetImage('assets/images/placeholder-image.png'), fit: BoxFit.cover)
+                                      )
+                                  ),
+                                  SizedBox(height: 5),
+                                ]
+                            )
+                          ]
+                      ),
+                      Row (
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    padding: new EdgeInsets.only(right: 13.0),
+                                    width: 200,
+                                    child: new Text(
+                                      product.name,
+                                      overflow: TextOverflow.fade,
+                                      style: new TextStyle(
+                                        fontSize: 14.0,
+                                        fontFamily: 'Roboto',
+                                        fontWeight: FontWeight.normal,
                                       ),
                                     ),
-                                    SizedBox(height: 5),
-                                    Text(formatter.format(double.parse(data.price).round()),
-                                      style: new TextStyle(
-                                        fontSize: 12.0,
-                                        fontFamily: 'Roboto',
-                                        fontWeight: FontWeight.bold,
-                                      )
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(formatter.format(double.parse(product.price).round()),
+                                    style: new TextStyle(
+                                      fontSize: 12.0,
+                                      fontFamily: 'Roboto',
+                                      fontWeight: FontWeight.bold,
                                     )
-                                  ]
-                              ),
-                              Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 70,
-                                      child: Align(
-                                        alignment: Alignment.centerRight,
-                                        child: calculateDistance(currentLocation.latitude, currentLocation.longitude, data.latitude, data.longitude)
-                                      )
+                                  )
+                                ]
+                            ),
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    width: 70,
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: calculateDistance(currentLocation.latitude, currentLocation.longitude, product.latitude, product.longitude)
                                     )
-                                  ]
-                              )
-                            ]
-                        )
+                                  )
+                                ]
+                            )
+                          ]
+                      )
                   ],
                 )
             )
-          )
+          ),
+          onTapDown: (TapDownDetails details) {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ProductPage(product)));
+          },
         )
       )
     );
---
+++ flutter/lib/constants/url-config.dart
@@ -0,0 +1,1 @@
+const String baseImagePath = 'https://s3-us-west-2.amazonaws.com/resold-photos/catalog/product';
