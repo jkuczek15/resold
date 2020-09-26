d9851a46d3f504cd1d9d7a85a4e340ea1a80e221
Joe Kuczek <joe@resold.us>
2020-09-26 12:50:50 -0500

    postmates delivery quote

--- flutter/lib/view-models/request/postmates/delivery-quote-request.dart
+++ flutter/lib/view-models/request/postmates/delivery-quote-request.dart
@@ -2,16 +2,19 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'delivery-quote-request.g.dart';
 
-@JsonSerializable(nullable: false)
+@JsonSerializable(nullable: true)
 class DeliveryQuoteRequest {
 
+  // dropoff info
   final String dropoff_address;
-  final String pickup_address;
   final DateTime dropoff_deadline_dt;
   final double dropoff_latitude;
   final double dropoff_longitude;
   final String dropoff_phone_number;
   final DateTime dropoff_ready_dt;
+
+  // pickup info
+  final String pickup_address;
   final DateTime pickup_deadline_dt;
   final double pickup_latitude;
   final double pickup_longitude;
@@ -20,4 +23,7 @@ class DeliveryQuoteRequest {
 
   DeliveryQuoteRequest({this.dropoff_address, this.pickup_address, this.dropoff_deadline_dt, this.dropoff_latitude, this.dropoff_longitude,
   this.dropoff_phone_number, this.dropoff_ready_dt, this.pickup_deadline_dt, this.pickup_latitude, this.pickup_longitude, this.pickup_phone_number, this.pickup_ready_dt});
+
+  factory DeliveryQuoteRequest.fromJson(Map<String, dynamic> json) => _$DeliveryQuoteRequestFromJson(json);
+  Map<String, dynamic> toJson() => _$DeliveryQuoteRequestToJson(this);
 }
---
+++ flutter/lib/view-models/response/postmates/delivery-quote-response.dart
@@ -0,0 +1,25 @@
+import 'package:resold/view-models/response/abstract-response.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'delivery-quote-response.g.dart';
+
+@JsonSerializable(nullable: true)
+class DeliveryQuoteResponse extends Response {
+
+  final String id;                   // delivery quote id
+  final DateTime created;
+  final String currency;
+  final String currency_type;
+  final DateTime dropoff_eta;
+  final int duration;            // estimated minutes for delivery to reach drop-off
+  final DateTime expires;
+  final int fee;                // amount in cents that will be charged if delivery is created
+  final String kind;
+  final int pickup_duration;    // estimated minutes until a courier will arrive at the pickup
+
+  DeliveryQuoteResponse({this.id, this.created, this.currency, this.currency_type, this.dropoff_eta, this.duration, this.expires, this.fee, this.kind, this.pickup_duration,
+    status, error}) : super(status: status, error: error);
+
+  factory DeliveryQuoteResponse.fromJson(Map<String, dynamic> json) => _$DeliveryQuoteResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$DeliveryQuoteResponseToJson(this);
+}
