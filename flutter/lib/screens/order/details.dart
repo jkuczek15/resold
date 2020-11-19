4059f3dfcefd55cf86da1da0402afa34ed55fe5e
Joe Kuczek <joe.kuczek@gmail.com>
2020-11-18 20:46:21 -0600

    profile picture

--- flutter/lib/screens/order/details.dart
+++ flutter/lib/screens/order/details.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
