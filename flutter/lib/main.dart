ffd034cd8ff47aebc396e613e610f9f98cff5e61
Joe Kuczek <joe@resold.us>
2020-11-22 10:24:26 -0600

    add address popup

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -37,11 +37,11 @@ Future<void> main() async {
       androidPayMode: env.stripeAndroidPayMode));
 
   // clear from disk
-  // await CustomerResponse.clear();
+  await CustomerResponse.clear();
 
   // auto-login
-  await CustomerResponse.save(CustomerResponse(email: 'joe.kuczek@gmail.com', password: 'Resold420!'));
-  // await CustomerResponse.save(CustomerResponse(email: 'jim.smith@gmail.com', password: 'Resold420!'));
+  // await CustomerResponse.save(CustomerResponse(email: 'joe.kuczek@gmail.com', password: 'Resold420!'));
+  await CustomerResponse.save(CustomerResponse(email: 'jim.smith@gmail.com', password: 'Resold420!'));
   // await CustomerResponse.save(CustomerResponse(email: 'bob.smith@gmail.com', password: 'Resold420!'));
 
   // get from disk and login
