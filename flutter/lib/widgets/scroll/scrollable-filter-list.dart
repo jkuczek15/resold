4a273a3e85632f9d479236ca0a3ac5e2359de9fd
Joe Kuczek <joe.kuczek@gmail.com>
2020-11-22 17:45:32 -0600

    remove distance tick marks

--- flutter/lib/widgets/scroll/scrollable-filter-list.dart
+++ flutter/lib/widgets/scroll/scrollable-filter-list.dart
@@ -34,7 +34,7 @@ class ScrollableFilterListState extends State<ScrollableFilterList> {
     'Cancel': MdiIcons.close,
   };
   List basesortlist = ['Newest', '(\$) Low to High', '(\$) High to Low'];
-  double _currentSliderValue = 30;
+  double _currentSliderValue = 25;
   List<bool> checkedSortList = [true, false, false];
 
   TextStyle categoryTextStyle = new TextStyle();
@@ -61,135 +61,127 @@ class ScrollableFilterListState extends State<ScrollableFilterList> {
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              PopupMenuButton<PopupMenuItem>(
-                  child: dropdownCategory,
-                  onSelected: selectedCategory,
-                  itemBuilder: (BuildContext context) {
-                    var categorylist = List<PopupMenuItem>();
-                    categoriesMap.forEach((t, i) => categorylist.add(
-                          PopupMenuItem(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(i),
-                                SizedBox(width: 10),
-                                Text(t),
-                              ],
-                            ),
-                            value: PopupMenuItem(value: t, child: Text(t)),
-                          ),
-                        ));
-                    return categorylist.map((PopupMenuItem choice) {
-                      return PopupMenuItem<PopupMenuItem>(
-                        value: choice,
-                        child: choice,
-                      );
-                    }).toList();
-                  }),
-              PopupMenuButton<PopupMenuItem>(
-                  child: Column(
-                    children: [
-                      Icon(MdiIcons.mapMarkerRadius),
-                      Text('Distance'),
-                    ],
-                  ),
-                  itemBuilder: (BuildContext context) {
-                    return <PopupMenuEntry<PopupMenuItem<Slider>>>[
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+          PopupMenuButton<PopupMenuItem>(
+              child: dropdownCategory,
+              onSelected: selectedCategory,
+              itemBuilder: (BuildContext context) {
+                var categorylist = List<PopupMenuItem>();
+                categoriesMap.forEach((t, i) => categorylist.add(
                       PopupMenuItem(
-                        enabled: false,
-                        child: StatefulBuilder(builder:
-                            (BuildContext context, StateSetter setState) {
-                          var miles = _currentSliderValue.toInt();
-                          return Column(children: [
-                            Text('$miles mi'),
-                            Slider(
-                              value: _currentSliderValue > 100
-                                  ? 100
-                                  : _currentSliderValue,
-                              min: 5,
-                              max: 100,
-                              divisions: 19,
-                              activeColor: ResoldBlue,
-                              inactiveColor: Colors.grey[300],
-                              label: _currentSliderValue.round().toString(),
-                              onChanged: (double value) {
-                                setState(() {
-                                  _currentSliderValue = value;
-                                });
-                              },
-                            ),
-                          ]);
-                        }),
-                      )
-                    ];
-                  }),
-              PopupMenuButton<PopupMenuItem>(
-                  child: dropdownCondition,
-                  onSelected: selectedCondition,
-                  itemBuilder: (BuildContext context) {
-                    var conditionList = List<PopupMenuItem>();
-                    conditionMap.forEach((t, i) => conditionList.add(
-                          PopupMenuItem(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(i),
-                                SizedBox(width: 10),
-                                Text(t),
-                              ],
-                            ),
-                            value: PopupMenuItem(value: t, child: Text(t)),
-                          ),
-                        ));
-                    return conditionList.map((PopupMenuItem choice) {
-                      return PopupMenuItem<PopupMenuItem>(
-                        value: choice,
-                        child: choice,
-                      );
-                    }).toList();
-                  }),
-              PopupMenuButton<PopupMenuItem>(
-                  child: Column(
-                    children: [
-                      Icon(MdiIcons.sort),
-                      Text('Sort'),
-                    ],
-                  ),
-                  onSelected: selectedSort,
-                  itemBuilder: (BuildContext context) {
-                    var sortlist = List.generate(
-                      basesortlist.length,
-                      (index) => PopupMenuItem<PopupMenuItem>(
-                        value: PopupMenuItem(
-                            value: index, child: Text(index.toString())),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text(basesortlist[index]),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Icon(
-                              MdiIcons.check,
-                              color: checkedSortList[index]
-                                  ? ResoldBlue
-                                  : Colors.white,
-                            ),
+                            Icon(i),
+                            SizedBox(width: 10),
+                            Text(t),
                           ],
                         ),
+                        value: PopupMenuItem(value: t, child: Text(t)),
                       ),
-                    );
-                    return sortlist.map((PopupMenuItem choice) {
-                      return PopupMenuItem<PopupMenuItem>(
-                        value: choice,
-                        child: choice,
-                      );
-                    }).toList();
-                  }),
-            ]));
+                    ));
+                return categorylist.map((PopupMenuItem choice) {
+                  return PopupMenuItem<PopupMenuItem>(
+                    value: choice,
+                    child: choice,
+                  );
+                }).toList();
+              }),
+          PopupMenuButton<PopupMenuItem>(
+              child: Column(
+                children: [
+                  Icon(MdiIcons.mapMarkerRadius),
+                  Text('Distance'),
+                ],
+              ),
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry<PopupMenuItem<Slider>>>[
+                  PopupMenuItem(
+                    enabled: false,
+                    child: StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+                      var miles = _currentSliderValue.toInt();
+                      return Column(children: [
+                        Text('$miles mi'),
+                        Slider(
+                          value: _currentSliderValue,
+                          min: 5,
+                          max: 50,
+                          divisions: 9,
+                          activeColor: ResoldBlue,
+                          inactiveColor: Colors.grey[300],
+                          label: _currentSliderValue.round().toString(),
+                          onChanged: (double value) {
+                            setState(() {
+                              _currentSliderValue = value;
+                            });
+                          },
+                        ),
+                      ]);
+                    }),
+                  )
+                ];
+              }),
+          PopupMenuButton<PopupMenuItem>(
+              child: dropdownCondition,
+              onSelected: selectedCondition,
+              itemBuilder: (BuildContext context) {
+                var conditionList = List<PopupMenuItem>();
+                conditionMap.forEach((t, i) => conditionList.add(
+                      PopupMenuItem(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(i),
+                            SizedBox(width: 10),
+                            Text(t),
+                          ],
+                        ),
+                        value: PopupMenuItem(value: t, child: Text(t)),
+                      ),
+                    ));
+                return conditionList.map((PopupMenuItem choice) {
+                  return PopupMenuItem<PopupMenuItem>(
+                    value: choice,
+                    child: choice,
+                  );
+                }).toList();
+              }),
+          PopupMenuButton<PopupMenuItem>(
+              child: Column(
+                children: [
+                  Icon(MdiIcons.sort),
+                  Text('Sort'),
+                ],
+              ),
+              onSelected: selectedSort,
+              itemBuilder: (BuildContext context) {
+                var sortlist = List.generate(
+                  basesortlist.length,
+                  (index) => PopupMenuItem<PopupMenuItem>(
+                    value: PopupMenuItem(value: index, child: Text(index.toString())),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(basesortlist[index]),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          MdiIcons.check,
+                          color: checkedSortList[index] ? ResoldBlue : Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+                return sortlist.map((PopupMenuItem choice) {
+                  return PopupMenuItem<PopupMenuItem>(
+                    value: choice,
+                    child: choice,
+                  );
+                }).toList();
+              }),
+        ]));
   }
 
   void selectedCategory(PopupMenuItem choice) async {
