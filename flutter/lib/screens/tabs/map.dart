1e8a98abd8c92e02582eacba3d6ce5ad160fe8f3
Joe Kuczek <joe@resold.us>
2020-11-22 18:04:26 -0600

    update map with search bar

--- flutter/lib/screens/tabs/map.dart
+++ flutter/lib/screens/tabs/map.dart
@@ -1,11 +1,17 @@
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:rebloc/rebloc.dart';
+import 'package:resold/constants/ui-constants.dart';
+import 'package:resold/models/product.dart';
+import 'package:resold/services/search.dart';
 import 'package:resold/state/app-state.dart';
 import 'package:resold/view-models/response/magento/customer-response.dart';
 import 'package:resold/widgets/loading.dart';
+import 'package:resold/widgets/scroll/scrollable-filter-list.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
@@ -17,6 +23,9 @@ class MapPage extends StatefulWidget {
 class MapPageState extends State<MapPage> {
   Future<Position> futureCurrentLocation;
   final Map<String, Marker> markers = {};
+  List<Product> products = new List<Product>();
+  Future<List<Product>> futureLocalProducts;
+  String searchTerm = '';
 
   @override
   void initState() {
@@ -33,16 +42,68 @@ class MapPageState extends State<MapPage> {
               future: futureCurrentLocation,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Container(
-                      height: 600,
-                      child: GoogleMap(
-                        onMapCreated: (GoogleMapController controller) => onMapCreated(controller, snapshot.data),
-                        initialCameraPosition: CameraPosition(
-                          target: LatLng(snapshot.data.latitude, snapshot.data.longitude),
-                          zoom: 9.0,
-                        ),
-                        markers: markers.values.toSet(),
-                      ));
+                  Position currentLocation = snapshot.data;
+                  futureLocalProducts =
+                      Search.fetchSearchProducts(searchTerm, currentLocation.latitude, currentLocation.longitude);
+                  return LiquidPullToRefresh(
+                    height: 80,
+                    springAnimationDurationInMilliseconds: 500,
+                    onRefresh: () {
+                      // setState(() {});
+                      // return futureLocalProducts = Search.fetchSearchProducts(
+                      //     searchTerm, currentLocation.latitude, currentLocation.longitude);
+                    },
+                    showChildOpacityTransition: false,
+                    color: ResoldBlue,
+                    animSpeedFactor: 5.0,
+                    child: FutureBuilder<List<Product>>(
+                      future: futureLocalProducts,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Stack(
+                            children: [
+                              Container(
+                                  height: 650,
+                                  child: GoogleMap(
+                                    onMapCreated: (GoogleMapController controller) =>
+                                        onMapCreated(controller, currentLocation),
+                                    initialCameraPosition: CameraPosition(
+                                      target: LatLng(currentLocation.latitude, currentLocation.longitude),
+                                      zoom: 9.0,
+                                    ),
+                                    markers: markers.values.toSet(),
+                                  )),
+                              Container(
+                                color: Colors.white.withOpacity(0.9),
+                                height: 130,
+                                child: SearchBar<Product>(
+                                  header: ScrollableFilterList(),
+                                  hintText: 'Search entire marketplace here...',
+                                  searchBarPadding: EdgeInsets.symmetric(horizontal: 20),
+                                  cancellationWidget: Icon(Icons.cancel),
+                                  onSearch: (term) {
+                                    searchTerm = term;
+                                    products = new List<Product>();
+                                    return Search.fetchSearchProducts(
+                                        term, currentLocation.latitude, currentLocation.longitude);
+                                  },
+                                  loader: Center(child: Loading()),
+                                  suggestions: snapshot.data,
+                                  onItemFound: (Product product, int index) {
+                                    products.add(product);
+                                    return SizedBox();
+                                  },
+                                  emptyWidget: Center(child: Text('Your search returned no results.')),
+                                ),
+                              ),
+                            ],
+                          );
+                        } else {
+                          return Center(child: Loading());
+                        } // end if we have data
+                      },
+                    ),
+                  );
                 } else {
                   return Center(child: Loading());
                 } // end if snapshot has data
