66e9a8847a03e678fcd62932b2b297fa2039056c
Joe Kuczek <joe.kuczek@gmail.com>
2020-11-23 21:06:42 -0600

    add conditioin and category helper

---
+++ flutter/lib/helpers/category-helper.dart
@@ -0,0 +1,24 @@
+class CategoryHelper {
+  static int getCategoryIdByName(String name) {
+    switch (name) {
+      case 'Electronics':
+        return 42;
+      case 'Fashion':
+        return 93;
+      case 'Home & Lawn':
+        return 100;
+      case 'Outdoors':
+        return 101;
+      case 'Sporting Goods':
+        return 102;
+      case 'Music':
+        return 103;
+      case 'Collectibles':
+        return 104;
+      case 'Handmade':
+        return 106;
+      default:
+        return 0;
+    } // end switch case
+  } // end function getCategoryIdByName
+}
