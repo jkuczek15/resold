ea4c02c8dc34360f85b6ac5d9d78a3657ac5c226
Joe Kuczek <joe@resold.us>
2020-12-08 13:39:45 -0600

    run yum install in before script instead of after

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,3 +8,19 @@ fi
 # Remove all web files
 rm -rf /var/www/html/* || true
 rm -rf /var/www/html/.* || true
+
+# Solve conflicts between epel and ami packages
+yum-config-manager --disable epel
+yum clean all
+yum update -y
+yum-config-manager --enable epel
+
+# Install gcc and C++ 
+yum -y install gcc72 gcc72-c++
+
+# Install PECL and gRPC extension 
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install php70-devel php7-pear zlib-devel
+pecl7 install grpc
+pecl7 install protobuf
