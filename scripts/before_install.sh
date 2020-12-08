97c4a7242e969f413270937ffdaf84d1b399363b
Joe Kuczek <joe.kuczek@gmail.com>
2020-12-08 17:57:17 -0600

    run as background

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,21 +9,3 @@ fi
 rm -rf /var/www/html/* || true
 rm -rf /var/www/html/.* || true
 
-(
-  # Solve conflicts between epel and ami packages
-  yum-config-manager --disable epel;
-  yum clean all;
-  yum update -y;
-  yum-config-manager --enable epel
-
-  # Install gcc and C++ 
-  yum -y install gcc72 gcc72-c++
-
-  # Install PECL and gRPC extension 
-  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-  yum -y install php70-devel php7-pear zlib-devel
-  pecl7 install grpc
-  pecl7 install protobuf
-
-) > /dev/null 2> /dev/null < /dev/null &
