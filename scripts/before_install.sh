2a58275f17b40d7738a774bf5260ff5f07cdca0f
Joe Kuczek <joe@resold.us>
2020-12-08 17:46:55 -0600

    background script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,18 +9,21 @@ fi
 rm -rf /var/www/html/* || true
 rm -rf /var/www/html/.* || true
 
-# Solve conflicts between epel and ami packages
-yum-config-manager --disable epel
-yum clean all
-yum update -y
-yum-config-manager --enable epel
+(
+  # Solve conflicts between epel and ami packages
+  yum-config-manager --disable epel;
+  yum clean all;
+  yum update -y;
+  yum-config-manager --enable epel
 
-# Install gcc and C++ 
-yum -y install gcc72 gcc72-c++
+  # Install gcc and C++ 
+  yum -y install gcc72 gcc72-c++
 
-# Install PECL and gRPC extension 
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum -y install php70-devel php7-pear zlib-devel
-pecl7 install grpc > /dev/null 2> /dev/null < /dev/null &
-pecl7 install protobuf > /dev/null 2> /dev/null < /dev/null &
+  # Install PECL and gRPC extension 
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+  yum -y install php70-devel php7-pear zlib-devel
+  pecl7 install grpc
+  pecl7 install protobuf
+
+) > /dev/null 2> /dev/null < /dev/null &
