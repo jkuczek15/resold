b0dda660cecf66e275c9b553b7e6e5c56dfd4311
Danielle Chamberlain <089daniellechambers@gmail.com>
2020-12-07 21:38:57 -0600

    sell form steps

--- flutter/lib/screens/home.dart
+++ flutter/lib/screens/home.dart
@@ -28,12 +28,14 @@ import 'package:resold/state/actions/set-customer.dart';
 import 'package:resold/state/actions/set-selected-tab.dart';
 import 'package:resold/state/app-state.dart';
 import 'package:resold/state/search-state.dart';
+import 'package:resold/state/sell-state.dart';
 import 'package:resold/view-models/firebase/inbox-message.dart';
 import 'package:resold/view-models/response/magento/customer-response.dart';
 import 'package:resold/widgets/loading.dart';
 
 class Home extends StatelessWidget {
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey(debugLabel: 'Main Navigator');
+  final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey(debugLabel: 'Main Navigator');
 
   // This widget is the root of your application.
   @override
@@ -70,7 +72,8 @@ class Home extends StatelessWidget {
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold,
                                   ),
-                                  tickMarkShape: SliderTickMarkShape.noTickMark),
+                                  tickMarkShape:
+                                      SliderTickMarkShape.noTickMark),
                               scaffoldBackgroundColor: Colors.white,
                               brightness: Brightness.light,
                               accentColor: Colors.white,
@@ -124,14 +127,16 @@ class HomePageState extends State<HomePage> {
           children: [
             Align(
                 alignment: Alignment.centerLeft,
-                child: Image.asset('assets/images/resold-white-logo.png', width: 145, height: 145)),
+                child: Image.asset('assets/images/resold-white-logo.png',
+                    width: 145, height: 145)),
             Align(
               alignment: Alignment.centerRight,
               child: InkWell(
                 child: StreamBuilder(
                     stream: ResoldFirebase.getUnreadMessageCount(customer.id),
                     builder: (context, snapshot) {
-                      if (snapshot.hasData && snapshot.data.documents.length != 0) {
+                      if (snapshot.hasData &&
+                          snapshot.data.documents.length != 0) {
                         return Stack(
                           children: <Widget>[
                             Icon(Icons.message, color: Colors.white),
@@ -166,8 +171,10 @@ class HomePageState extends State<HomePage> {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              InboxPage(customer: customer, currentLocation: currentLocation, dispatcher: dispatcher)));
+                          builder: (context) => InboxPage(
+                              customer: customer,
+                              currentLocation: currentLocation,
+                              dispatcher: dispatcher)));
                 },
               ),
               // child: Icon(Icons.message, color: Colors.white),
@@ -187,15 +194,18 @@ class HomePageState extends State<HomePage> {
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
-          BottomNavigationBarItem(icon: Icon(Icons.attach_money), label: 'Sell'),
-          BottomNavigationBarItem(icon: Icon(MdiIcons.truck), label: 'Deliveries'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money), label: 'Sell'),
+          BottomNavigationBarItem(
+              icon: Icon(MdiIcons.truck), label: 'Deliveries'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account')
         ],
         currentIndex: selectedTab.index,
         backgroundColor: Colors.white,
         fixedColor: ResoldBlue,
         unselectedItemColor: Colors.black,
-        onTap: (int index) => dispatcher(SetSelectedTabAction(SelectedTab.values[index])),
+        onTap: (int index) =>
+            dispatcher(SetSelectedTabAction(SelectedTab.values[index])),
       ),
     );
   } // end function build
@@ -211,7 +221,8 @@ class HomePageState extends State<HomePage> {
                   initialData: searchState.initialProducts,
                   stream: searchState.searchStream.stream,
                   builder: (context, snapshot) {
-                    List<Product> results = snapshot.hasData ? snapshot.data : [];
+                    List<Product> results =
+                        snapshot.hasData ? snapshot.data : [];
                     if (selectedTab == SelectedTab.map) {
                       return MapPage(
                           customer: customer,
@@ -230,7 +241,22 @@ class HomePageState extends State<HomePage> {
                   });
             });
       case SelectedTab.sell:
-        return SellPage(customer: customer, currentLocation: currentLocation, dispatcher: dispatcher);
+        return ViewModelSubscriber<AppState, SellState>(
+            converter: (state) => state.sellState,
+            builder: (context, dispatcher, sellState) {
+              return SellPage(
+                  customer: customer,
+                  currentLocation: currentLocation,
+                  listingTitleController: sellState.listingTitleController,
+                  priceController: sellState.priceController,
+                  detailsController: sellState.detailsController,
+                  selectedCondition: sellState.selectedCondition,
+                  selectedCategory: sellState.selectedCategory,
+                  selectedItemSize: sellState.selectedItemSize,
+                  currentFormStep: sellState.currentFormStep,
+                  focusState: sellState.focusState,
+                  dispatcher: dispatcher);
+            });
       case SelectedTab.orders:
         return OrdersPage(customer: customer);
       case SelectedTab.account:
@@ -322,19 +348,22 @@ class HomePageState extends State<HomePage> {
     var chatId = data['chatId'];
     if (chatId != null) {
       // normal message notification
-      InboxMessage inboxMessage = await ResoldFirebase.getUserInboxMessage(chatId);
-      CustomerResponse toCustomer = await Magento.getCustomerById(inboxMessage.toId);
+      InboxMessage inboxMessage =
+          await ResoldFirebase.getUserInboxMessage(chatId);
+      CustomerResponse toCustomer =
+          await Magento.getCustomerById(inboxMessage.toId);
 
       // open message page
-      Navigator.of(scaffoldKey.currentContext, rootNavigator: true).push(MaterialPageRoute(
-          builder: (context) => MessagePage(
-              fromCustomer: customer,
-              toCustomer: toCustomer,
-              currentLocation: currentLocation,
-              product: inboxMessage.product,
-              chatId: chatId,
-              type: inboxMessage.messageType,
-              dispatcher: dispatcher)));
+      Navigator.of(scaffoldKey.currentContext, rootNavigator: true).push(
+          MaterialPageRoute(
+              builder: (context) => MessagePage(
+                  fromCustomer: customer,
+                  toCustomer: toCustomer,
+                  currentLocation: currentLocation,
+                  product: inboxMessage.product,
+                  chatId: chatId,
+                  type: inboxMessage.messageType,
+                  dispatcher: dispatcher)));
     } else {
       // delivery event notification
       int orderId = int.tryParse(data['orderId']);
@@ -345,8 +374,10 @@ class HomePageState extends State<HomePage> {
       Product product = await ResoldRest.getProduct(customer.token, productId);
 
       // navigate to order page
-      Navigator.of(scaffoldKey.currentContext, rootNavigator: true)
-          .push(MaterialPageRoute(builder: (context) => OrderDetails(order: order, product: product, isSeller: false)));
+      Navigator.of(scaffoldKey.currentContext, rootNavigator: true).push(
+          MaterialPageRoute(
+              builder: (context) => OrderDetails(
+                  order: order, product: product, isSeller: false)));
     } // end if type is message
     Navigator.of(context, rootNavigator: true).pop('dialog');
   } // end function navigateFromNotification
---
+++ flutter/lib/state/sell-state.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:resold/state/sell-focus-state.dart';
+
+class SellState {
+  TextEditingController listingTitleController;
+  TextEditingController priceController;
+  TextEditingController detailsController;
+  int selectedCategory;
+  int selectedCondition;
+  int selectedItemSize;
+  int currentFormStep;
+  PageController formPageViewController;
+  SellFocusState focusState;
+
+  SellState(
+      {this.listingTitleController,
+      this.priceController,
+      this.detailsController,
+      this.selectedCategory,
+      this.selectedCondition,
+      this.selectedItemSize,
+      this.currentFormStep,
+      this.formPageViewController,
+      this.focusState});
+}
