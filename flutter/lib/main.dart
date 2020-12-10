5b55919fcc29110a7b6e025d27dbd2e2ff7889a2
Joe Kuczek <joe@resold.us>
2020-12-10 11:51:45 -0600

    fix deliveries scroll

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -55,7 +55,7 @@ Future<void> main() async {
   if (env.isDevelopment) {
     // clear from disk
     await CustomerResponse.clear();
-    await CustomerResponse.save(TestAccounts.seller);
+    await CustomerResponse.save(TestAccounts.buyer);
   } // end if development
 
   // get from disk and login
