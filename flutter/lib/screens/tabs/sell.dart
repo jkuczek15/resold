ceaa1779b518c61fa56f7480c4ee27907bbc8515
Joe Kuczek <joe@resold.us>
2020-12-09 20:30:04 -0600

    remove scroll view

--- flutter/lib/screens/tabs/sell.dart
+++ flutter/lib/screens/tabs/sell.dart
@@ -314,16 +314,16 @@ class SellPage extends StatelessWidget {
         ),
       ),
       Container(
-          child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 0, 20),
+                  padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
                   child: Text(
                     'Select Category',
                     style: TextStyle(color: Colors.black, fontSize: 16),
@@ -522,7 +522,7 @@ class SellPage extends StatelessWidget {
                 ]),
                 SizedBox(height: 20),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 10),
                   child: Text(
                     'Select Vehicle Required',
                     style: TextStyle(color: Colors.black, fontSize: 16),
@@ -642,7 +642,7 @@ class SellPage extends StatelessWidget {
             ),
           ],
         ),
-      )),
+      ),
       Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
