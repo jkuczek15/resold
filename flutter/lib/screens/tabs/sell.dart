b33292e2bc04c4375f9678386955c7a924ff2c3e
Joe Kuczek <joe@resold.us>
2020-12-10 22:55:52 -0600

    fix carousel

--- flutter/lib/screens/tabs/sell.dart
+++ flutter/lib/screens/tabs/sell.dart
@@ -757,14 +757,15 @@ class SellPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             CarouselSlider(
-                options: CarouselOptions(height: 350.0),
+                options: CarouselOptions(height: 400.0),
                 items: sellState.imageState.images.map((image) {
                   return Builder(builder: (BuildContext context) {
                     return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                      child: AssetThumb(asset: image, width: 200, height: 200),
-                    );
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                        child: FadeInImage(
+                            image: AssetThumbImageProvider(image, width: 400, height: 500),
+                            placeholder: AssetImage('assets/images/placeholder-image.png')));
                   });
                 }).toList()),
             Row(
