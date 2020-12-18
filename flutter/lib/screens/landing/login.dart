30d99da2f7bc956512fdcf5c1d2d2c0d798b7d8f
Joe Kuczek <joe.kuczek@gmail.com>
2020-12-17 19:47:24 -0600

    render overflow on landing page

--- flutter/lib/screens/landing/login.dart
+++ flutter/lib/screens/landing/login.dart
@@ -3,7 +3,6 @@ import 'package:resold/constants/ui-constants.dart';
 import 'package:resold/screens/home.dart';
 import 'package:resold/services/magento.dart';
 import 'package:resold/services/resold-firebase.dart';
-import 'package:resold/services/resold.dart';
 import 'package:resold/state/actions/init-state.dart';
 import 'package:resold/state/app-state.dart';
 import 'package:resold/view-models/request/magento/login-request.dart';
@@ -34,153 +33,148 @@ class LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
         child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            resizeToAvoidBottomPadding: false,
             body: Stack(children: [
-          Image.asset('assets/images/login/resold-app-loginpage-background.jpg', fit: BoxFit.cover, width: 500),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(children: [
-                  Padding(
-                      child: Align(
-                          alignment: Alignment.topCenter,
-                          child: Image.asset('assets/images/resold-white-logo.png', fit: BoxFit.cover, width: 500)),
-                      padding: EdgeInsets.fromLTRB(30, 0, 30, 20)),
-                  Center(
-                      child: Text('Buy & sell with on-demand delivery',
-                          style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)))
-                ]),
-                Center(
-                    child: Column(children: [
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
-                      child: TextField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                              hintText: 'Enter your email...',
-                              hintStyle: TextStyle(color: Colors.white),
-                              labelStyle: new TextStyle(
-                                color: ResoldBlue,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 1.5),
-                              ),
-                              focusedBorder:
-                                  UnderlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1.5)),
-                              border: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 1.5),
-                              )),
-                          style: TextStyle(color: Colors.white))),
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(50, 10, 50, 30),
-                      child: TextField(
-                          obscureText: true,
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                              hintText: 'Enter your password...',
-                              hintStyle: TextStyle(color: Colors.white),
-                              labelStyle: new TextStyle(
-                                color: ResoldBlue,
-                              ),
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 1.5),
-                              ),
-                              focusedBorder:
-                                  UnderlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1.5)),
-                              border: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white, width: 1.5),
-                              )),
-                          style: TextStyle(color: Colors.white))),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(8)),
-                    onPressed: () async {
-                      // show a loading indicator
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Center(child: Loading());
-                          });
+              Image.asset('assets/images/login/resold-app-loginpage-background.jpg', fit: BoxFit.cover, width: 500),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(children: [
+                      Padding(
+                          child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Image.asset('assets/images/resold-white-logo.png', fit: BoxFit.cover, width: 500)),
+                          padding: EdgeInsets.fromLTRB(30, 0, 30, 20)),
+                      Center(
+                          child: Text('Buy & sell with on-demand delivery',
+                              style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)))
+                    ]),
+                    Center(
+                        child: Column(children: [
+                      Padding(
+                          padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
+                          child: TextField(
+                              controller: emailController,
+                              decoration: InputDecoration(
+                                  hintText: 'Enter your email...',
+                                  hintStyle: TextStyle(color: Colors.white),
+                                  labelStyle: new TextStyle(
+                                    color: ResoldBlue,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                                  ),
+                                  focusedBorder:
+                                      UnderlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                                  border: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                                  )),
+                              style: TextStyle(color: Colors.white))),
+                      Padding(
+                          padding: EdgeInsets.fromLTRB(50, 10, 50, 30),
+                          child: TextField(
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: InputDecoration(
+                                  hintText: 'Enter your password...',
+                                  hintStyle: TextStyle(color: Colors.white),
+                                  labelStyle: new TextStyle(
+                                    color: ResoldBlue,
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                                  ),
+                                  focusedBorder:
+                                      UnderlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                                  border: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                                  )),
+                              style: TextStyle(color: Colors.white))),
+                      RaisedButton(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(8)),
+                        onPressed: () async {
+                          // show a loading indicator
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Center(child: Loading());
+                              });
 
-                      // attempt to login
-                      CustomerResponse customer = await Magento.loginCustomer(
-                          LoginRequest(username: emailController.text, password: passwordController.text));
+                          // attempt to login
+                          CustomerResponse customer = await Magento.loginCustomer(
+                              LoginRequest(username: emailController.text, password: passwordController.text));
 
-                      if (customer.statusCode == 200) {
-                        // login was successful
-                        // store to disk
-                        await CustomerResponse.save(customer);
+                          if (customer.statusCode == 200) {
+                            // login was successful
+                            // store to disk
+                            await CustomerResponse.save(customer);
 
-                        // create a firebase user
-                        await ResoldFirebase.createOrUpdateUser(customer);
+                            // create a firebase user
+                            await ResoldFirebase.createOrUpdateUser(customer);
 
-                        // initialize application state
-                        // todo: init state on sign-in
-                        await Future.wait([
-                          Resold.getVendor(customer.vendorId),
-                          Resold.getVendorProducts(customer.vendorId, 'for-sale'),
-                          Resold.getVendorProducts(customer.vendorId, 'sold')
-                        ]).then((data) async {
-                          dispatcher(InitStateAction(await AppState.initialState(customer)));
-                        });
+                            // initialize application state
+                            dispatcher(InitStateAction(await AppState.initialState(customer)));
 
-                        // navigate
-                        Navigator.of(context, rootNavigator: true).pop('dialog');
-                        Navigator.pop(context);
-                        Navigator.pushReplacement(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, animation, secondaryAnimation) => Home(),
-                              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                return FadeTransition(opacity: animation, child: child);
-                              },
-                            ));
-                      } else {
-                        Navigator.of(context, rootNavigator: true).pop('dialog');
-                        return showDialog<void>(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Sign In Error'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[Text(customer.error)],
-                                ),
-                              ),
-                              actions: <Widget>[
-                                FlatButton(
-                                  child: Text('Ok'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
+                            // navigate
+                            Navigator.of(context, rootNavigator: true).pop('dialog');
+                            Navigator.pop(context);
+                            Navigator.pushReplacement(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder: (context, animation, secondaryAnimation) => Home(),
+                                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                    return FadeTransition(opacity: animation, child: child);
                                   },
-                                ),
-                              ],
+                                ));
+                          } else {
+                            Navigator.of(context, rootNavigator: true).pop('dialog');
+                            return showDialog<void>(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Sign In Error'),
+                                  content: SingleChildScrollView(
+                                    child: ListBody(
+                                      children: <Widget>[Text(customer.error)],
+                                    ),
+                                  ),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text('Ok'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
                             );
-                          },
-                        );
-                      }
-                    },
-                    child: Text('Sign In',
-                        style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
-                    padding: EdgeInsets.fromLTRB(105, 30, 105, 30),
-                    color: Colors.black,
-                    textColor: Colors.white,
-                  ),
-                  SizedBox(height: 15),
-                  InkWell(
-                    onTap: forgotPassword,
-                    child: Text('Forgot password?',
-                        style: new TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            decoration: TextDecoration.underline)),
-                  ),
-                  SizedBox(height: 10)
-                ])),
-                SizedBox(height: 5)
-              ])
-        ])),
+                          }
+                        },
+                        child: Text('Sign In',
+                            style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
+                        padding: EdgeInsets.fromLTRB(105, 30, 105, 30),
+                        color: Colors.black,
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(height: 15),
+                      InkWell(
+                        onTap: forgotPassword,
+                        child: Text('Forgot password?',
+                            style: new TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                decoration: TextDecoration.underline)),
+                      ),
+                      SizedBox(height: 10)
+                    ])),
+                    SizedBox(height: 5)
+                  ])
+            ])),
         onWillPop: () async {
           Navigator.pop(context);
           return false;
